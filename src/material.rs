a4461680312a175318a57b400a278eb1e357a2eb
Martin Kavík <martin@kavik.cz>
2021-05-18 22:28:27 +0200

    fixed reflectance

--- src/material.rs
+++ src/material.rs
@@ -74,7 +74,7 @@ impl Dielectric {
     fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
         // Use Schlick's approximation for reflectance
         let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
-        r0 * (1.0 - r0) * (1.0 - cosine).powi(5)
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
     }
 }
 
