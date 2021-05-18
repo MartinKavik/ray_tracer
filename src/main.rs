21c5a3840e7da20132bf6a8ec3450507452477bf
Martin Kavík <martin@kavik.cz>
2021-05-18 15:44:35 +0200

    sky, fixed first render loop

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
-mod vec3;
-use vec3::{Vec3, Color};
+mod ray;
+mod vec;
+
+use vec::{Vec3, Point3, Color};
+use ray::Ray;
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().normalized();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() -> () {
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0
+                            - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("255");
 
-    for j in 0..IMAGE_HEIGHT {
-        eprintln!("Scanlines remaining: {}", IMAGE_HEIGHT - j - 1);
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b = 0.25;
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
 
-            let ir = (255.999 * r) as u64;
-            let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b) as u64;
+            let r = Ray::new(origin,
+                             lower_left_corner + u * horizontal + v * vertical - origin);
+            let pixel_color = ray_color(&r);
 
-            println!("{} {} {}", ir, ig, ib);
+            println!("{}", pixel_color.format_color());
         }
     }
 }
