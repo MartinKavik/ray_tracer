0770daf1bcccc85b75c1f915f42ddd3055e6b7f9
Martin Kavík <martin@kavik.cz>
2021-05-18 21:37:17 +0200

    final scene

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,62 @@ use rand::Rng;
 use material::{Lambertian, Metal, Dielectric};
 use std::rc::Rc;
 
+fn random_scene() -> World {
+    let mut rng = rand::thread_rng();
+    let mut world = World::new();
+
+    let ground_mat = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    let ground_sphere = Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_mat);
+
+    world.push(Box::new(ground_sphere));
+
+    for a in -11..=11 {
+        for b in -11..=11 {
+            let choose_mat: f64 = rng.gen();
+            let center = Point3::new((a as f64) + rng.gen_range(0.0..0.9),
+                                     0.2,
+                                     (b as f64) + rng.gen_range(0.0..0.9));
+
+            if choose_mat < 0.8 {
+                // Diffuse
+                let albedo = Color::random(0.0..1.0) * Color::random(0.0..1.0);
+                let sphere_mat = Rc::new(Lambertian::new(albedo));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            } else if choose_mat < 0.95 {
+                // Metal
+                let albedo = Color::random(0.4..1.0);
+                let fuzz = rng.gen_range(0.0..0.5);
+                let sphere_mat = Rc::new(Metal::new(albedo, fuzz));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            } else {
+                // Glass
+                let sphere_mat = Rc::new(Dielectric::new(1.5));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            }
+        }
+    }
+
+    let mat1 = Rc::new(Dielectric::new(1.5));
+    let mat2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    let mat3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+
+    let sphere1 = Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat1);
+    let sphere2 = Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat2);
+    let sphere3 = Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat3);
+
+    world.push(Box::new(sphere1));
+    world.push(Box::new(sphere2));
+    world.push(Box::new(sphere3));
+
+    world
+}
+
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
         // If we've exceeded the ray bounce limit, no more light is gathered
@@ -35,47 +91,29 @@ fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
 
 fn main() -> () {
     // Image
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u64 = 256;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const IMAGE_WIDTH: u64 = 1200;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
-    const SAMPLES_PER_PIXEL: u64 = 100;
-    const MAX_DEPTH: u64 = 5;
+    const SAMPLES_PER_PIXEL: u64 = 500;
+    const MAX_DEPTH: u64 = 50;
 
     // World
-    let mut world = World::new();
-
-    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let mat_left = Rc::new(Dielectric::new(1.5));
-    let mat_left_inner = Rc::new(Dielectric::new(1.5));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
-
-    let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
-    let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
-    let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
-    let sphere_left_inner = Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.45, mat_left_inner);
-    let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
-
-    world.push(Box::new(sphere_ground));
-    world.push(Box::new(sphere_center));
-    world.push(Box::new(sphere_left));
-    world.push(Box::new(sphere_left_inner));
-    world.push(Box::new(sphere_right));
+    let world = random_scene();
 
     // Camera
-    let lookfrom = Point3::new(3.0, 3.0, 2.0);
-    let lookat = Point3::new(0.0, 0.0, -1.0);
+    let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let cam = Camera::new(lookfrom,
-                        lookat,
-                        vup,
-                        20.0,
-                        ASPECT_RATIO,
-                        aperture,
-                        dist_to_focus);
+                          lookat,
+                          vup,
+                          20.0,
+                          ASPECT_RATIO,
+                          aperture,
+                          dist_to_focus);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
